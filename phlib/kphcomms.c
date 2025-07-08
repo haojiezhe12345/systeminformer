e52789a9366a3f2af0a6154dd758c683bae96944
Johnny Shaw <johnny.shaw@live.com>
2025-07-07 21:53:36 -0600

    kph comms SAL cleanup

--- phlib/kphcomms.c
+++ phlib/kphcomms.c
@@ -87,6 +87,7 @@ VOID KphpCommsCallbackUnhandled(
  * \param[in] IoSB Result of the asynchronous I/O operation.
  * \param[in,out] Io Unused
  */
+_Function_class_(TP_IO_CALLBACK)
 VOID WINAPI KphpCommsIoCallback(
     _Inout_ PTP_CALLBACK_INSTANCE Instance,
     _Inout_opt_ PVOID Context,
@@ -214,6 +215,7 @@ Exit:
     return FALSE;
 }
 
+_Function_class_(USER_THREAD_START_ROUTINE)
 NTSTATUS NTAPI KphpRingBufferProcessor(
     _In_ PVOID Context
     )
@@ -498,7 +500,6 @@ BOOLEAN KphCommsIsConnected(
  *
  * \return Successful or errant status.
  */
-_Use_decl_annotations_
 NTSTATUS KphCommsReplyMessage(
     _In_ ULONG_PTR ReplyToken,
     _In_ PKPH_MESSAGE Message
@@ -570,7 +571,7 @@ CleanupExit:
  *
  * \return Successful or errant status.
  */
-_Use_decl_annotations_
+_Must_inspect_result_
 NTSTATUS KphCommsSendMessage(
     _Inout_ PKPH_MESSAGE Message
     )
